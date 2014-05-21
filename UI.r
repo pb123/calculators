a2eb7a36384e95678b9bec9456d8a7d298589601
pb123 <patrick@betterment.com>
2014-05-21 13:13:47 -0400

    app working with logit summary and 2 inputs

--- UI.r
+++ UI.r
@@ -6,9 +6,16 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Miles Per Gallon"),
-  sidebarPanel(),
-  mainPanel( numericInput("visits", "Experiences per Day:", 6000, 0),
-             verbatimTextOutput("visits")
-             )
+  headerPanel("Conversion Test Calculator"),
+  sidebarPanel(numericInput(inputId = "n1", label="Experiences 1", value = 0, min = 0, step=1),
+          
+               numericInput(inputId = "x1", label="Conversions 1", value = 0, min = 0, step=1),
+        
+               numericInput(inputId = "n2", label="Experiences 2", value = 0, min = 0, step=1),
+  
+               numericInput(inputId = "x2", label="Conversions 2", value = 0, min = 0, step=1),
+       
+               submitButton("Update View")),
+  mainPanel(h4("Summary"),
+            verbatimTextOutput("summary"))
 ))
--- server.r
+++ server.r
@@ -2,8 +2,34 @@ library(shiny)
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
-  output$visits <- renderPrint({
-    visits <- input$visits
-    cat(visits)
+ 
+  generateRawData <- function(data){
+    n1 <- data[1]; x1 <- data[2]; n2 <- data[3]; x2 <- data[4];
+    df <- data.frame(signup=c(rep(0,n1-x1),rep(1,x1),rep(0,n2-x2),rep(1,x2)), 
+                     test=factor(c(rep(1,n1), rep(2,n2))))
+    df
+  }
+  
+
+  output$summary <- renderPrint({
+    data <- c(input$n1,input$x1,input$n2,input$x2)
+
+    
+    
+    #     n1 <- input$n1
+    #     n2 <- input$n2
+    #     x1 <- input$x1
+    #     x2 <- input$x2
+    
+  
+    df <- generateRawData(data)
+    
+    logit1 <- (glm(signup ~ test, data = df, family = "binomial"))
+    
+    
+    summary(logit1)
+   
   })
+  
+  
 })
